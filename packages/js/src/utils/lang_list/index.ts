cdb1fc518a30075d9ce7e900e560306459b3e7a5
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-11-19 19:37:28 +0530

    Initialize Test Data (#1)
    * init script data
    
    * gen init test data done
    
    * added script name normalizaler

---
+++ packages/js/src/utils/lang_list/index.ts
@@ -0,0 +1,45 @@
+// ALWAYS BE CAREFUL BEFORE CHANGING LANG JSON
+// AS CHNAGE IN PRE-EXISTING LANG IDS WOULD CAUSE DATA MISMATCHs
+
+import langs from './langs.json';
+const { script_list, lang_list } = langs;
+
+export const SCRIPT_LIST = Object.keys(script_list);
+export type script_list_type = keyof typeof script_list;
+export const SCRIPT_LIST_IDS = Object.values(script_list);
+
+export const get_script_from_id = (id: number) => {
+  return SCRIPT_LIST[SCRIPT_LIST_IDS.indexOf(id)] as script_list_type;
+};
+
+export const LANG_LIST = Object.keys(lang_list);
+export const LANG_LIST_IDS = Object.values(lang_list);
+export type lang_list_type = keyof typeof lang_list;
+// the langs enum in schema.ts has to be updated manually
+
+export const get_lang_from_id = (id: number) => {
+  return LANG_LIST[LANG_LIST_IDS.indexOf(id)] as lang_list_type;
+};
+
+export type script_and_lang_list_type = script_list_type | lang_list_type;
+export const ALL_LANG_SCRIPT_LIST = Array.from(new Set([...LANG_LIST, ...SCRIPT_LIST])) as script_and_lang_list_type[];
+
+export { lang_list as lang_list_obj, script_list as script_list_obj };
+
+export const LANG_SCRIPT_MAP: Record<lang_list_type, script_list_type> = {
+  Bengali: 'Bengali',
+  Gujarati: 'Gujarati',
+  Hindi: 'Devanagari',
+  Kannada: 'Kannada',
+  Malayalam: 'Malayalam',
+  Odia: 'Odia',
+  Sinhala: 'Sinhala',
+  Tamil: 'Tamil',
+  Telugu: 'Telugu',
+  English: 'Normal',
+  Sanskrit: 'Devanagari',
+  Marathi: 'Devanagari',
+  Nepali: 'Devanagari',
+  Punjabi: 'Gurumukhi',
+  Assamese: 'Assamese'
+};
--- scripts/gen-test-data.ts
+++ scripts/gen-test-data.ts
@@ -1,6 +1,116 @@
 #!/usr/bin/env bun
 
-import { lipi_parivartak } from './tools/old_lipi_lekhika';
+import { lipi_parivartak as old_lipi_parivartak } from './tools/old_lipi_lekhika';
+import * as fs from 'node:fs';
+import { type script_list_type } from '../packages/js/src/utils/lang_list';
+import chalk from 'chalk';
+import path from 'node:path';
 
-const text = await lipi_parivartak('namO bhUtanAtham', 'en', 'hi');
-console.log(text);
+type TestData = {
+  index: number;
+  from: script_list_type;
+  to: script_list_type;
+  input: string;
+  output: string;
+  reversible: boolean;
+};
+
+const DEVANAGARI_INPUTS = [
+  'ऋषीणां संनिधौ राजंस्तव पुत्रागमं प्रति ।\nकाश्यपस्य च पुत्रोऽस्ति विभाण्डक इति श्रुतः ॥१-९-३॥',
+  'वक्ष्यन्ति ते महीपालं ब्राह्मणा वेदपारगाः ।\nविभाण्डकसुतं राजन् सर्वोपायैरिहानय ॥१-९-१२॥',
+  'तां ददर्श महातेजा मेनकां कुशिकात्मजः ।\nरूपेणाप्रतिमां तत्र विद्युतं जलदे यथा ॥१-६३-५॥',
+  'इमं विवस्वते योगं प्रोक्तवानहमव्ययम् ।\nविवस्वान्मनवे प्राह मनुरिक्ष्वाकवेऽब्रवीत् ॥४-१॥'
+];
+
+/**
+ * Generates test Test Data for transliteration to and from Devangari to other Brahmic scripts
+ */
+const devangari_other_brahmic_scripts = async () => {
+  const OUT_FILE_NAME = 'devangari_other_brahmic_scripts';
+  const FROM_SCRIPT = 'Devanagari';
+  const OTHER_BRAHMI_SCRIPTS = [
+    'Gujarati',
+    'Bengali',
+    'Assamese',
+    'Odia',
+    'Telugu',
+    'Kannada',
+    'Malayalam',
+    'Sinhala'
+  ] satisfies script_list_type[];
+
+  const out_test_data: TestData[] = [];
+
+  let index = 0;
+  for (const input of DEVANAGARI_INPUTS) {
+    for (const other_script of OTHER_BRAHMI_SCRIPTS) {
+      const output = await old_lipi_parivartak(input, FROM_SCRIPT, other_script);
+      const test_data: TestData = {
+        index: index++,
+        from: FROM_SCRIPT,
+        to: other_script,
+        input: input,
+        output: output,
+        reversible: true
+      };
+      out_test_data.push(test_data);
+    }
+  }
+  fs.writeFileSync(
+    path.join(__dirname, '..', 'test_data', OUT_FILE_NAME + '.yaml'),
+    // @ts-ignore
+    Bun.YAML.stringify(out_test_data, null, 2)
+  );
+  // Using a tick mark sign (✓) in the printed message
+  console.log(chalk.green.bold(`✓  Devanagari ⇆ Other Brahmic Scripts`));
+};
+
+/**
+ * Generates test Test Data for transliteration to and from Devangari to non-Brahmic scripts (Normal and Romanized)
+ */
+const devanagari_non_brahmic_scripts = async () => {
+  const OUT_FILE_NAME = 'devangari_non_brahmic_scripts';
+  const FROM_SCRIPT = 'Devanagari';
+  const NON_BRAHMI_SCRIPTS = ['Normal', 'Romanized'] satisfies script_list_type[];
+  const out_test_data: TestData[] = [];
+  let index = 0;
+  for (const input of DEVANAGARI_INPUTS) {
+    for (const non_brahmic_script of NON_BRAHMI_SCRIPTS) {
+      const output = await old_lipi_parivartak(input, FROM_SCRIPT, non_brahmic_script);
+      out_test_data.push({
+        index: index++,
+        from: FROM_SCRIPT,
+        to: non_brahmic_script,
+        input: input,
+        output: output.replace(/\.(?=\d)/g, ''), // handling the case of number conversion where १ -> 1 (not .1)
+        reversible: false
+      });
+      // this version is reversible as १ -> .1 and .1 -> १
+      out_test_data.push({
+        index: index++,
+        from: non_brahmic_script,
+        to: FROM_SCRIPT,
+        input: output,
+        output: input,
+        reversible: true
+      });
+    }
+  }
+  fs.writeFileSync(
+    path.join(__dirname, '..', 'test_data', OUT_FILE_NAME + '.yaml'),
+    // @ts-ignore
+    Bun.YAML.stringify(out_test_data, null, 2)
+  );
+  // Using a tick mark sign (✓) in the printed message
+  console.log(chalk.green.bold(`✓  Devanagari ⇆ Non-Brahmic Scripts`));
+};
+
+/**
+ * This generates the required test data for the projec
+ */
+async function main() {
+  console.log(chalk.yellow.bold('Generating test data...'));
+  await Promise.all([devangari_other_brahmic_scripts(), devanagari_non_brahmic_scripts()]);
+}
+
+main();
