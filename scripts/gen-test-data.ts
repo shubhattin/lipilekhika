db4685b4223b004db742551714d433ef48364857
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-11-19 19:10:26 +0530

    gen init test data done

--- scripts/gen-test-data.ts
+++ scripts/gen-test-data.ts
@@ -15,11 +15,18 @@ type TestData = {
   reversible: boolean;
 };
 
+const DEVANAGARI_INPUTS = [
+  'ऋषीणां संनिधौ राजंस्तव पुत्रागमं प्रति ।\nकाश्यपस्य च पुत्रोऽस्ति विभाण्डक इति श्रुतः ॥१-९-३॥',
+  'वक्ष्यन्ति ते महीपालं ब्राह्मणा वेदपारगाः ।\nविभाण्डकसुतं राजन् सर्वोपायैरिहानय ॥१-९-१२॥',
+  'तां ददर्श महातेजा मेनकां कुशिकात्मजः ।\nरूपेणाप्रतिमां तत्र विद्युतं जलदे यथा ॥१-६३-५॥',
+  'इमं विवस्वते योगं प्रोक्तवानहमव्ययम् ।\nविवस्वान्मनवे प्राह मनुरिक्ष्वाकवेऽब्रवीत् ॥४-१॥'
+];
+
 /**
  * Generates test Test Data for transliteration to and from Devangari to other Brahmic scripts
  */
 const devangari_other_brahmic_scripts = async () => {
-  const OUT_FILE_NAME = 'devangari_other_brahmic_scripts.json';
+  const OUT_FILE_NAME = 'devangari_other_brahmic_scripts';
   const FROM_SCRIPT = 'Devanagari';
   const OTHER_BRAHMI_SCRIPTS = [
     'Gujarati',
@@ -31,7 +38,6 @@ const devangari_other_brahmic_scripts = async () => {
     'Malayalam',
     'Sinhala'
   ] satisfies script_list_type[];
-  const DEVANAGARI_INPUTS = ['अ'];
 
   const out_test_data: TestData[] = [];
 
@@ -51,11 +57,52 @@ const devangari_other_brahmic_scripts = async () => {
     }
   }
   fs.writeFileSync(
-    path.join(__dirname, '..', 'test_data', OUT_FILE_NAME),
-    JSON.stringify(out_test_data, null, 2)
+    path.join(__dirname, '..', 'test_data', OUT_FILE_NAME + '.yaml'),
+    // @ts-ignore
+    Bun.YAML.stringify(out_test_data, null, 2)
   );
   // Using a tick mark sign (✓) in the printed message
   console.log(chalk.green.bold(`✓  Devanagari ⇆ Other Brahmic Scripts`));
+};
+
+/**
+ * Generates test Test Data for transliteration to and from Devangari to non-Brahmic scripts (Normal and Romanized)
+ */
+const devanagari_non_brahmic_scripts = async () => {
+  const OUT_FILE_NAME = 'devangari_non_brahmic_scripts';
+  const FROM_SCRIPT = 'Devanagari';
+  const NON_BRAHMI_SCRIPTS = ['Normal', 'Romanized'] satisfies script_list_type[];
+  const out_test_data: TestData[] = [];
+  let index = 0;
+  for (const input of DEVANAGARI_INPUTS) {
+    for (const non_brahmic_script of NON_BRAHMI_SCRIPTS) {
+      const output = await old_lipi_parivartak(input, FROM_SCRIPT, non_brahmic_script);
+      out_test_data.push({
+        index: index++,
+        from: FROM_SCRIPT,
+        to: non_brahmic_script,
+        input: input,
+        output: output.replace(/\.(?=\d)/g, ''), // handling the case of number conversion where १ -> 1 (not .1)
+        reversible: false
+      });
+      // this version is reversible as १ -> .1 and .1 -> १
+      out_test_data.push({
+        index: index++,
+        from: non_brahmic_script,
+        to: FROM_SCRIPT,
+        input: output,
+        output: input,
+        reversible: true
+      });
+    }
+  }
+  fs.writeFileSync(
+    path.join(__dirname, '..', 'test_data', OUT_FILE_NAME + '.yaml'),
+    // @ts-ignore
+    Bun.YAML.stringify(out_test_data, null, 2)
+  );
+  // Using a tick mark sign (✓) in the printed message
+  console.log(chalk.green.bold(`✓  Devanagari ⇆ Non-Brahmic Scripts`));
 };
 
 /**
@@ -63,7 +110,7 @@ const devangari_other_brahmic_scripts = async () => {
  */
 async function main() {
   console.log(chalk.yellow.bold('Generating test data...'));
-  await Promise.all([devangari_other_brahmic_scripts()]);
+  await Promise.all([devangari_other_brahmic_scripts(), devanagari_non_brahmic_scripts()]);
 }
 
 main();
