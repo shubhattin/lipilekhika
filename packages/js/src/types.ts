b7fb33090a3e8cfec5c54097263565a6f24fef67
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-11-20 08:54:05 +0530

    init types

--- packages/js/src/types.ts
+++ packages/js/src/types.ts
@@ -1,6 +1,38 @@
+import type { KramaKeysType } from './interlang_array_keys';
+
+/*
+ * These types are not the final types to be used by conversion engine.
+ * These are defined for organzing the script data
+ * For actual application use it will compiled into a more direct usable format
+ */
+
 type BrahmicScriptType = {
   script_type: 'brahmic';
-  list: any;
+  halant: string;
+  nuqta?: string;
+  list: ({
+    key: string;
+    /** This containts duplicates of the key, for eg:- some nuqta sysmbols and mAtrAs */
+    duplicates?: string[];
+    /** In case if no entry is found for the key in krama even after deplucation resolution
+     * then fallback array will be used. Like for malayalam .100 it can be .1+.0+.0
+     */
+    fallback?: KramaKeysType[];
+  } & (
+    | { type: 'svara'; mAtrA: string; mAtrA_duplicates?: string[] }
+    | { type: 'vyanjana' | 'anya' }
+  ))[];
+  /*
+   * In Brahmic Scripts
+   - svara -> vowels
+   - vyanjana -> consonants
+   - anya -> other
+  */
 };
 
-export type ScriptInfoType = BrahmicScriptType;
+type OtherScriptType = {
+  script_type: 'other';
+  list: { key: string }[];
+};
+
+export type ScriptInfoType = BrahmicScriptType | OtherScriptType;
