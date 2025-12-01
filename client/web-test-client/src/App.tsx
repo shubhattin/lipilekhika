da3e32fbac3ac54ba605002eccb84bd6629628d6
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-12-01 17:19:05 +0530

    init solid

---
+++ client/web-test-client/src/App.tsx
@@ -0,0 +1,26 @@
+import { createSignal } from "solid-js";
+
+function App() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <>
+      <div>
+        <h1>Web Test Client</h1>
+      </div>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Solid logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
