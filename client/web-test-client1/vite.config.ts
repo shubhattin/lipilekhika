602330394fb72023d5f6380dc6ce9bc61f7d2f80
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-12-13 15:49:30 +0530

    rm solids

--- client/web-test-client1/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import solid from 'vite-plugin-solid';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-  base: '/lipilekhika/',
-  plugins: [tailwindcss(), solid()]
-});
