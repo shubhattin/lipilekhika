be2de45d6ae4aff0e482ac092bdb59eb5897936f
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-12-17 10:13:02 +0530

    added svelte app

--- docs/docs-and-web-client/astro.config.mjs
+++ docs/docs-and-web-client/astro.config.mjs
@@ -7,6 +7,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/lipilekhika/',
   integrations: [
     starlight({
       title: 'Docs with Tailwind',
