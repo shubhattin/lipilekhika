e165065b435f9fe83e44d932b84183b953345451
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-12-19 09:52:10 +0530

    script data caching in umd

--- packages/js/src/utils/get_script_data.ts
+++ packages/js/src/utils/get_script_data.ts
@@ -7,19 +7,26 @@ import {
 
 const IS_UMD_BUILD_MODE = get_is_umd_build_mode_macro();
 
+const UMD_SCRIPT_DATA_CACHE: {
+  [script_name in script_list_type]?: OutputScriptData;
+} = {};
+
 /**
  * Gets the script data for a given script name
  * @param script_name - The name of the script to get the data for
  * @returns The script data
  */
 export const getScriptData = async (script_name: script_list_type): Promise<OutputScriptData> => {
-  // @ts-ignore
   if (IS_UMD_BUILD_MODE) {
+    if (UMD_SCRIPT_DATA_CACHE[script_name]) {
+      return UMD_SCRIPT_DATA_CACHE[script_name];
+    }
     const package_current_version = get_package_current_version_macro();
     const SCRIPT_DATA_URL = `https://cdn.jsdelivr.net/npm/lipilekhika@${package_current_version}/dist/umd_json/script_data/${script_name}.json`;
     const response = await fetch(SCRIPT_DATA_URL);
-    const data = await response.json();
-    return data as OutputScriptData;
+    const data = (await response.json()) as OutputScriptData;
+    UMD_SCRIPT_DATA_CACHE[script_name] = data;
+    return data;
   }
   const scriptData = (await import(`../script_data/${script_name}.json`))
     .default as OutputScriptData;
