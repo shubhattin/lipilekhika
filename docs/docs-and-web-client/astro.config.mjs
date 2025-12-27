ab695f51b460f84d0c3a6c1a3c6e3791da7e5c3d
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-12-27 21:36:48 +0530

    reoved subpath mount as bow we use

--- docs/docs-and-web-client/astro.config.mjs
+++ docs/docs-and-web-client/astro.config.mjs
@@ -8,7 +8,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/lipilekhika/',
   trailingSlash: 'never',
   integrations: [
     starlight({
