3d2ca551d0976e121e4971d541e18a241af03021
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-12-27 20:14:56 +0530

    simplified examples

--- docs/typing_tool_usage/astro.config.mjs
+++ docs/typing_tool_usage/astro.config.mjs
@@ -10,7 +10,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/lipilekhika/typing_tool_examples/',
+  base: '/lipilekhika/typing_tool_examples',
   trailingSlash: 'never',
   integrations: [
     svelte(),
