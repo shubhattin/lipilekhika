cce61d3535233377a9f2b7282255b5d3ecccea4f
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-12-31 16:33:28 +0530

    remember

--- docs/docs-and-web-client/src/components/script/state.ts
+++ docs/docs-and-web-client/src/components/script/state.ts
@@ -1,6 +1,10 @@
 import { atom } from 'nanostores';
 import type { ScriptListType } from 'lipilekhika';
 
+const LOCAL_STORAGE_KEYS = {
+  TYPING_SCRIPT: 'lipilekhika-app-typing-script'
+} as const;
+
 /** Shared state for input text
  *
  * Used for both `HomePageTyping` and `MainApp` components.
@@ -11,4 +15,14 @@ const DEFAULT_FROM: ScriptListType = 'Devanagari';
 /**
  * Shared state for typing script
  */
-export const typing_script_atom = atom<ScriptListType>(DEFAULT_FROM);
+export const typing_script_atom = atom<ScriptListType>(
+  (() => {
+    if (typeof window === 'undefined') return DEFAULT_FROM;
+    const localStorageValue = localStorage.getItem(LOCAL_STORAGE_KEYS.TYPING_SCRIPT);
+    return localStorageValue ? (localStorageValue as ScriptListType) : DEFAULT_FROM;
+  })()
+);
+typing_script_atom.subscribe((value) => {
+  if (typeof window === 'undefined') return;
+  localStorage.setItem(LOCAL_STORAGE_KEYS.TYPING_SCRIPT, value);
+});
