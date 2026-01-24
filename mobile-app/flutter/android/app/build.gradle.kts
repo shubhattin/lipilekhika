aa8202d666c30f215bac56143bf0665b9843f41a
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-01-24 07:49:04 +0530

    init

--- mobile-app/flutter/android/app/build.gradle.kts
+++ mobile-app/flutter/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "in.lipilekhika.lipilekhika_flutter"
+    namespace = "com.shubhattin.lipilekhika"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "in.lipilekhika.lipilekhika_flutter"
+        applicationId = "com.shubhattin.lipilekhika"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
