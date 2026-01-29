7344df1b0a0e1593ac8840b960b7b57e5ea0ed34
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-01-29 14:53:50 +0530

    preset init

---
+++ docs/docs-and-web-client/src/tools/presets.ts
@@ -0,0 +1,58 @@
+import { type ScriptListType, type TransliterationOptions } from 'lipilekhika';
+
+type Rule = keyof TransliterationOptions;
+
+type PresetListType = 'none' | 'tsc_portal';
+type Preset = Record<
+  PresetListType,
+  {
+    label: string;
+    description?: string;
+    direct_apply_rules: Rule[];
+    conditional_rules: {
+      rule: Rule;
+      from: ScriptListType;
+      to: ScriptListType;
+    }[];
+  }
+>;
+
+/**
+ * Scripts in which option `brahmic_to_brahmic:replace_pancham_varga_varna_with_anusvAra`
+ * is to be enabled
+ */
+export const SCRIPTS_TO_REPLACE_WITH_ANUNASIK = ['Telugu', 'Kannada'];
+
+export const PRESETS: Preset = {
+  none: {
+    label: 'None',
+    description: 'No preset',
+    direct_apply_rules: [],
+    conditional_rules: []
+  },
+  tsc_portal: {
+    label: 'The Sanskrit Channel',
+    description: 'Preset for TSC Portal',
+    direct_apply_rules: [
+      'all_to_normal:remove_virAma_and_double_virAma',
+      'all_to_normal:replace_avagraha_with_a',
+      'all_to_normal:replace_pancham_varga_varna_with_n',
+      'all_to_sinhala:use_conjunct_enabling_halant'
+    ],
+    conditional_rules: SCRIPTS_TO_REPLACE_WITH_ANUNASIK.map((script) => ({
+      rule: 'brahmic_to_brahmic:replace_pancham_varga_varna_with_anusvAra',
+      from: 'Devanagari',
+      to: script as ScriptListType
+    }))
+  }
+};
+
+/**
+ * Custom Options used for Transliteration in this project
+ */
+export const CUSTOM_TRANS_OPTIONS = {
+  'all_to_normal:remove_virAma_and_double_virAma': true,
+  'all_to_normal:replace_avagraha_with_a': true,
+  'all_to_normal:replace_pancham_varga_varna_with_n': true,
+  'all_to_sinhala:use_conjunct_enabling_halant': true
+} as TransliterationOptions;
